22b7b07c9b90460efe82da527772a149279948ce
mikko <mikko@mikkos-Mac.local>
2015-04-27 13:24:57 -0700

    initial commits

---
+++ Lab23/Diary.swift
@@ -0,0 +1,13 @@
+//
+//  Diary.swift
+//  Lab23
+//
+//  Created by mikko on 27/04/15.
+//  Copyright (c) 2015 mikko. All rights reserved.
+//
+
+import Foundation
+
+class Diary: NSObject, NSCoding {
+    
+}
